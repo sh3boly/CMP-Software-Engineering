9181a33a6e9b0612f89ec7d88aff87642aec175a
sh3boly <117195948+sh3boly@users.noreply.github.com>
2024-02-19 23:18:17 +0200

    Solved all the TODOs
    createEmployee both BE and FE implemented
    DeleteEmployee both BE and FE implemented

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -8,8 +8,24 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  const id = req.params.id;
+  console.log(id);
+  res.status(204);
+  const index = employee.findIndex(employee => employee.id === id);
+  employee.splice(index);
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  
+  const {name, id} = req.body;
+  
+  const employeeExist = employee.find(employee => employee.id === id);
+  if(employeeExist){
+    res.status(400).send({message: "ERROR A User with the same ID already exists!"});
+
+  }else{
+  employee.push({id, name});
+  res.status(201);
+  }
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -31,22 +31,66 @@ function fetchEmployees() {
 
 // TODO
 // add event listener to submit button
-
+const submitButton = document.getElementById("submitBtn");
+submitButton.addEventListener("click", createEmployee);
 // TODO
 // add event listener to delete button
-
+const dataTB = document.getElementById("dataTable");
+dataTB.addEventListener('click', function(event) {
+  if(event.target.classList.contains('btn-sm')){
+    deleteEmployee(event);
+  }
+ 
+})
 // TODO
-function createEmployee (){
+function createEmployee (event){
   // get data from input field
   // send data to BE
   // call fetchEmployees
+  event.preventDefault();
+  const ID = document.getElementById("id").value; 
+  const Name = document.getElementById("name").value;
+
+  const employeeData = {
+    id: ID,
+    name: Name
+  };
+  //console.log(employeeData);
+  fetch('http://localhost:3000/api/v1/employee', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(employeeData)
+  })
+    .catch(error => console.log(error));
+  fetchEmployees();
+
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee (event){
   // get id
   // send id to BE
   // call fetchEmployees
+  //event.preventDefault();
+  const clickedDeleteCell = event.target.closest('td');
+  
+  const clickedRow = clickedDeleteCell.parentNode;
+  
+  const clickedIDCell = clickedRow.querySelector('td:first-child');
+
+  const employeeID = clickedIDCell.textContent;
+  fetch('http://localhost:3000/api/v1/employee/' + employeeID, {
+    method: 'DELETE',
+    
+  })
+    .catch(error => console.log(error));
+
+fetchEmployees();
+  
+
 }
 
-fetchEmployees()
+
+fetchEmployees();
